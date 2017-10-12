795f9f926e59baf53906f112e8a13db7d6de1b72
ideastudent <ideastudent@10.235.65.8>
2017-10-11 17:52:58 -0700

    Method introduction

---
+++ Main/src/Main1.java
@@ -0,0 +1,27 @@
+
+public class Main1{
+public static boolean jen;
+     public static void main(String []args){
+        System.out.println("Hello World");
+        x();
+        Main2.Printbool();
+        
+        
+     
+     }
+     public static boolean x(){
+         double g = 7.9;
+         
+         if(g == 7.9){
+             jen = true;
+         }
+         return jen;
+         }
+    
+     
+     
+     
+     }     
+
+     
+
---
+++ Main/src/Main2.java
@@ -0,0 +1,8 @@
+
+public class Main2 {
+public static boolean mathbool = Main1.x();
+
+public static void Printbool(){
+System.out.println(mathbool);
+}
+}
